f23da6e56e64dd5c9737e47e3481ea9037c4ca33
mikechess <mikecook450@gmail.com>
2024-02-08 14:32:11 -0500

    04.Store Page

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,7 +1,19 @@
-export default function Store() {
+import { Col, Row } from "react-bootstrap"
+import storeItems from "../data/items.json"
+import StoreItem from "../components/StoreItem"
+
+
+export function Store() {
   return (
-    <div>
-        <h1>Store</h1>
-    </div>
+    <>
+      <h1>Store</h1>
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {storeItems.map(item => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
   )
 }
